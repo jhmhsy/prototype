9d442096db2d40c0094c7c975ded37e76876dfda
Volcanorc <sirjone100@gmail.com>
2024-09-02 23:39:02 +0800

    new

--- resources/views/components/custom/format.blade.php
+++ resources/views/components/custom/format.blade.php
@@ -18,38 +18,40 @@
 
 <body class="bg-gray-900 text-white">
     <div>
-        <nav class="border-b border-white/10 flex items-center justify-between py-5 px-10"">
-                <div>
-                    <a href=" /">
-            <x-custom.main-logo /></a>
-    </div>
-    <div class="space-x-10">
-        <x-nav-link :active="true" href="#">Home</x-nav-link>
-        <x-nav-link href="#">Features</x-nav-link>
-        <x-nav-link href="#">Pricing</x-nav-link>
-        <x-nav-link href="#">Contact</x-nav-link>
-    </div>
-    <div>
-        @if (Route::has('login'))
-        @auth
-        <x-nav-link href="{{ url('/dashboard') }}">Dashboard</x-nav-link>
-        @else
-        <x-nav-link href="{{ route('login') }}">Log in</x-nav-link>
-        @if (Route::has('register'))
-        <x-nav-link href="{{ route('register') }}">Register</x-nav-link>
-        @endif
-        @endauth
-        @endif
-    </div>
-    </nav>
+        <nav class="border-b border-white/10 flex items-center justify-between py-5 px-10">
+            <div>
+                <a href=" /">
+                    <img src="{{ asset('images/gym-icon.png') }}" alt="Example Image" class="w-6 h-6 md:w-12 md:h-12">
 
-    <main class="mt-10 m-auto w-full">
-        <x-custom.main-sections></x-custom.main-sections>
-    </main>
+                </a>
+            </div>
+            <div class="space-x-10">
+                <x-nav-link :active="true" href="#">Home</x-nav-link>
+                <x-nav-link href="#">Features</x-nav-link>
+                <x-nav-link href="#">Pricing</x-nav-link>
+                <x-nav-link href="#">Contact</x-nav-link>
+            </div>
+            <div>
+                @if (Route::has('login'))
+                @auth
+                <x-nav-link href="{{ url('/dashboard') }}">Dashboard</x-nav-link>
+                @else
+                <x-nav-link href="{{ route('login') }}">Log in</x-nav-link>
+                @if (Route::has('register'))
+                <x-nav-link href="{{ route('register') }}">Register</x-nav-link>
+                @endif
+                @endauth
+                @endif
+            </div>
+        </nav>
 
-    <footer class="dark:bg-darkmode_light p-6 md:py-12 w-full">
-        <x-custom.footer-section></x-custom.footer-section>
-    </footer>
+        <main class="mt-10 m-auto w-full">
+            <x-custom.main-sections></x-custom.main-sections>
+        </main>
+
+        <footer class="dark:bg-darkmode_light p-6 md:py-12 w-full">
+            <x-custom.footer-section></x-custom.footer-section>
+        </footer>
     </div>
 </body>
 
--- resources/views/components/custom/main-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{ asset('images/gym-icon.png') }}" alt="Example Image" class="w-6 h-6 md:w-12 md:h-12"><a href="">
