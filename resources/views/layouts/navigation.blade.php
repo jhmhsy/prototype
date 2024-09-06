bf92a6eafee8ea97d3b8c46f89cb44ffb0b70a7c
Volcanorc <sirjone100@gmail.com>
2024-09-06 14:04:09 +0800

    fixed navbar burger ersponsive

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
             <!-- Settings Dropdown -->
             @if (Route::has('login'))
             @auth
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class=" sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
@@ -54,7 +54,10 @@
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                    class="inline-flex  p-2 rounded-md text-gray-400 dark:text-gray-500 
+                    hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 
+                    dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 
+                    dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -71,15 +74,18 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-red-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('dashboard')">
+                    {{ __('Dashboard') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -101,9 +107,11 @@
             @if (Route::has('register'))
             <x-nav-link href="{{ route('register') }}" class="text-sm font-medium hover:underline underline-offset-4">
                 Register</x-nav-link>
-            @endif
-            @endauth
-            @endif
+
+
         </div>
     </div>
+    @endif
+    @endauth
+    @endif
 </nav>
