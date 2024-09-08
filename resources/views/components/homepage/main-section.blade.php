7547eb15100f7a0f5b922e70fefabd215a4d05c1
Volcanorc <sirjone100@gmail.com>
2024-09-08 20:03:01 +0800

    fixed, gamay, mao ni e pull, mao ni updated gamay hahahahha

--- resources/views/components/homepage/main-section.blade.php
+++ resources/views/components/homepage/main-section.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <section class="w-full h-[75vh] dark:bg-darkmode_light text-textblack dark:text-textwhite dark:border-white/10 dark:border-white/10">
+    <section
+        class="w-full h-[75vh] dark:bg-darkmode_light text-textblack dark:text-textwhite dark:border-white/10 dark:border-white/10">
         <div class="inset-0 flex flex-col items-center justify-center px-4 pt-40 text-center  ">
             <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
                 Elevate Your Fitness Journey
@@ -16,31 +17,31 @@
                 <div class="mx-auto grid max-w-5xl gap-6 py-12 lg:grid-cols-3 lg:gap-12 ">
                     <div
                         class="flex flex-col items-center space-y-4 text-center border border/white-10 dark:border-white/30 dark:hover:border-coolblue px-6 py-5 rounded-2xl hover:border-coolblue group transition-colors duration-300">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
-                            <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046zm2.094 2.025" class="group-hover:text-coolblue transition-colors duration-300"/>
-                        </svg>
-                        class="flex flex-col items-center space-y-4 text-center border border/white-10 dark:border-white/30 dark:hover:border-coolblue px-6 py-5 rounded-2xl hover:border-coolblue group transition-colors duration-300">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
                             <path
                                 d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046zm2.094 2.025"
                                 class="group-hover:text-coolblue transition-colors duration-300" />
                         </svg>
+
+
                         <h3 class="text-xl font-bold group-hover:text-coolblue transition-colors duration-300">Easy
                             Booking</h3>
                         <p>Reserve your spot in your favorite classes with just a few clicks.</p>
                     </div>
                     <div
                         class="flex flex-col items-center space-y-4 text-center border border/white-10 dark:border-white/30 dark:hover:border-coolblue px-6 py-5 rounded-2xl hover:border-coolblue group transition-colors duration-300">
-                        class="flex flex-col items-center space-y-4 text-center border border/white-10 dark:border-white/30 dark:hover:border-coolblue px-6 py-5 rounded-2xl hover:border-coolblue group transition-colors duration-300">
+
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
                             class="bi bi-calendar-check" viewBox="0 0 16 16">
                             <path
                                 d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"
-                                class="group-hover:text-coolblue transition-colors duration-300" class="group-hover:text-coolblue transition-colors duration-300"/>
+                                class="group-hover:text-coolblue transition-colors duration-300"
+                                class="group-hover:text-coolblue transition-colors duration-300" />
                             <path
                                 d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"
-                                class="group-hover:text-coolblue transition-colors duration-300" class="group-hover:text-coolblue transition-colors duration-300"/>
+                                class="group-hover:text-coolblue transition-colors duration-300"
+                                class="group-hover:text-coolblue transition-colors duration-300" />
                         </svg>
                         <h3 class="text-xl font-bold group-hover:text-coolblue transition-colors duration-300">Class
                             Schedules</h3>
@@ -48,8 +49,11 @@
                     </div>
                     <div
                         class="flex flex-col items-center space-y-4 text-center border border/white-10 dark:border-white/30 dark:hover:border-coolblue px-6 py-5 rounded-2xl hover:border-coolblue group transition-colors duration-300">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-                            <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" class="group-hover:text-coolblue transition-colors duration-300"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                            class="bi bi-people" viewBox="0 0 16 16">
+                            <path
+                                d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"
+                                class="group-hover:text-coolblue transition-colors duration-300" />
                         </svg>
                         <h3 class="text-xl font-bold group-hover:text-coolblue transition-colors duration-300">Member
                             Management</h3>
