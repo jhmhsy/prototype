70549005bcb78f38bc52e528a40cc3c08753e596
Kit Jasper <henrym45319+github@gmail.com>
2024-09-09 11:39:58 +0800

    added header section to login, to make it easier for users to go back to homepage

--- resources/views/components/custom/nav-link.blade.php
+++ resources/views/components/custom/nav-link.blade.php
@@ -1,26 +1,10 @@
 @props(['active'])
 
 @php
-$classes = ($active ?? false)
-? 'inline-flex items-center px-1 pt-1
-border-b-2 border-indigo-400
-text-sm font-medium leading-5
-text-gray-900
-focus:outline-none
-transition duration-150 ease-in-out
-dark:text-gray-100
-dark:border-indigo-600'
-: 'inline-flex items-center px-1 pt-1
-border-b-2 border-transparent
-text-sm font-medium leading-5
-text-gray-500
-hover:text-black
-focus:outline-none
-focus:text-gray-700
-transition duration-150 ease-in-out
-dark:text-gray-400
-dark:hover:text-white
-dark:focus:text-gray-300';
+    $classes =
+        $active ?? false
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none transition duration-150 ease-in-out dark:text-gray-100 dark:border-indigo-600'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-black focus:outline-none focus:text-gray-700 transition duration-150 ease-in-out dark:text-gray-400 dark:hover:text-white dark:focus:text-gray-300';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
