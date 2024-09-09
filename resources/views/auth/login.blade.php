06800bf21757658d628a730e5b023e3f46baec65
Kit Jasper <henrym45319+github@gmail.com>
2024-09-09 11:39:58 +0800

    added header section to login, to make it easier for users to go back to homepage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
     <div class="flex min-h-screen flex-col ">
-
+        <x-homepage.header-section></x-homepage.header-section>
         <!-- Home button -->
-        <div class="flex items-center">
+        {{--<div class="flex items-center">
             <x-custom.nav-link href="{{ route('welcome') }}" class="text-sm font-medium hover:text-black m-4 px-7 ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor"
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor"
                     class="bi bi-arrow-return-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                         d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5" />
                 </svg>
                 <h2 class="inline-block rounded-lg bg-muted px-3 text-xl py-1 font-medium">Back</h2>
             </x-custom.nav-link>
-        </div>
+        </div>--}}
 
 
         <!-- Login Form -->
