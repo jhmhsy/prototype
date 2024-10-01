6eeca7185efe79da01afb7b6ada589a263b01fb0
Kit Jasper <henrym45319+github@gmail.com>
2024-10-01 15:39:30 +0800

    responsive stuff on main-section

--- resources/views/components/homepage/main-section.blade.php
+++ resources/views/components/homepage/main-section.blade.php
@@ -1,20 +1,25 @@
-<section class="relative min-h-screen w-full dark:border-white/10 px-10 bg-gradient-to-br dark:from-shade_9 dark:via-main dark:to-tint_1 from-tint_1 via-main to-shade_9">
-    <div class="inset-0 flex flex-col sm:flex-row items-center justify-between px-15 pt-40 pb-15">
-        <div class="text-white w-full">
-            <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+<section
+    class="items-center relative min-h-screen w-full dark:border-white/10 px-4 sm:px-10 bg-gradient-to-br dark:from-shade_9 dark:via-main dark:to-tint_1 from-tint_1 via-main to-shade_9">
+    <div
+        class="inset-0 flex flex-col sm:flex-row items-center justify-between px-4 sm:px-15 pt-20 sm:pt-40 pb-10 sm:pb-15">
+        <div class="text-white">
+            <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter">
                 Elevate Your Fitness Journey
             </h1>
-            <x-custom.paragraph :dark="true" class="max-w-[900px] md:text-xl lg:text-base xl:text-xl">
+            <x-custom.paragraph :dark="true"
+                class="max-w-full sm:max-w-[900px] text-base sm:text-xl md:text-lg lg:text-xl">
                 Our gym reservation system makes it easy to book your sessions, manage your membership, and
                 stay on top of your fitness goals.
             </x-custom.paragraph>
-            <div class="flex flex-col sm:flex-row space-x-2 my-5">
-                <x-custom.primary-button type="button"
-                    onclick="window.location.href = '{{ route('ticket.show') }}'">
+            <div class="flex flex-col w-1/2 sm:w-full sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 my-5">
+                <!-- First Button -->
+                <x-custom.primary-button type="button" onclick="window.location.href = '{{ route('ticket.show') }}'"
+                    class="w-auto xs:w-auto sm:w-auto md:w-auto">
                     Book Now
                 </x-custom.primary-button>
+                <!-- Second Button -->
                 <x-custom.primary-button type="button" onclick="window.location.href = '#equipment-section'"
-                    :active="request()->routeIs('reservation')" class="px-20 space-x-1 border">
+                    :active="request()->routeIs('reservation')" class="w-auto xs:w-auto sm:w-auto md:w-auto px-5 sm:px-7 space-x-1 border">
                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
                         class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
@@ -26,10 +31,21 @@
                 </x-custom.primary-button>
             </div>
         </div>
-        <div class="container px-4 md:px-6 m-auto">
-            <img class="rounded-xl"
+
+        <div class="container px-2 sm:px-4 md:px-6 m-auto">
+            <img class="rounded-xl w-full"
                 src="https://img.freepik.com/free-photo/low-angle-view-unrecognizable-muscular-build-man-preparing-lifting-barbell-health-club_637285-2497.jpg?t=st=1727743360~exp=1727746960~hmac=7e0bf7885ab8fcd6befaf8eea6232011cdfce301515c9d57d0ca7bd5c9ade0b1&w=740"
                 alt="man-with-barbel">
         </div>
     </div>
 </section>
+
+<style>
+    @media (max-width: 360px) {
+        .space-y-2> :not([hidden])~ :not([hidden]) {
+            --tw-space-y-reverse: 0;
+            margin-top: calc(0.5rem * calc(1 - var(--tw-space-y-reverse)));
+            margin-bottom: calc(0.5rem * var(--tw-space-y-reverse));
+        }
+    }
+</style>
