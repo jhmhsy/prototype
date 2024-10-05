6c961e0c915bd88ef93caf9bed72f2cc9212925e
Kit Jasper <henrym45319+github@gmail.com>
2024-10-05 19:24:47 +0800

    merge with jasonbranch

--- resources/views/components/forms/nav-link.blade.php
+++ resources/views/components/forms/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes = $active
         ? 'border-main text-main dark:text-tint_4 dark:border-main'  
-        : 'border-transparent text-shade_9 hover:text-main dark:text-tint_1 dark:border-transparent dark:hover:text-tint_4';  // Border & text for inactive links
+        : 'border-transparent text-shade_9 hover:text-main dark:text-tint_1 dark:hover:text-tint_4';  // Border & text for inactive links
     $defaults =
         'border-b-2 transition duration-150 ease-in-out inline-flex leading-5 font-medium text-sm items-center px-1 pt-1';  // General styles
 @endphp
