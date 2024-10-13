75d17c877956ee42e119f616bc93efe4e4affcf7
Kit Jasper <henrym45319+github@gmail.com>
2024-10-13 14:44:45 +0800

    more stuff

--- resources/views/subpages/reservation.blade.php
+++ resources/views/subpages/reservation.blade.php
@@ -18,7 +18,7 @@
                                     <x-forms.field :value="__('Name')" :errors="$errors->get('regular-name')"
                                         :for="'regular-name'">
                                         <input placeholder="Juan Dela Cruz" type="text" id="regular-name" name="name"
-                                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-main dark:bg-tint_3"
+                                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-main dark:bg-tint_3 {{ Auth::user() ? 'text-gray-400' : 'text-shade_9 dark:text-shade_9' }}"
                                             value="{{ Auth::user() ? Auth::user()->name : '' }}" @auth disabled @endauth
                                             required>
                                     </x-forms.field>
