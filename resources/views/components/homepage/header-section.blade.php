e68655af3bb188e07afe21c87df83a64d1d1bb42
Kit Jasper <henrym45319+github@gmail.com>
2024-11-05 20:51:27 +0800

    aaaa

--- resources/views/components/homepage/header-section.blade.php
+++ resources/views/components/homepage/header-section.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed top-0 z-10 flex flex-no-wrap items-center justify-between w-full px-6 py-4 uppercase">
     <div class="flex flex-row mr-13">
         <a href="{{ route('welcome') }}" class="logo">
-            <img src="{{ asset('images/logo colored.png') }}" width="50" height="50" alt="logo" class="transition-transform duration-300 hover:scale-110">
+            <img src="{{ asset('images/logo.png') }}" width="50" height="50" alt="logo" class="transition-transform duration-300 hover:scale-110">
         </a>
     </div>
 
