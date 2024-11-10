a4ff17b727797bcd883d774a0b7ce91d42b4993c
volcanorc <sirjone100@gmail.com>
2024-11-11 03:21:25 +0800

    asd

--- resources/views/administrator/users/index.blade.php
+++ resources/views/administrator/users/index.blade.php
@@ -2,12 +2,8 @@
 <x-dash-layout>
 
 
-<<<<<<< HEAD
-    <div class="flex flex-col gap-4 rounded-lg border shadow-sm p-6  dark:border-white/5"
-=======
     <div class="flex flex-col gap-4 rounded-lg border shadow-sm p-6  text-shade_9  
                                 border-shade_6/50 dark:border-white/5"
->>>>>>> 29a4e95c4972173aedb72540c8402f7a5b0bceed
         x-data="{ openUserId: null, openshowmodal: null, opencreatemodal: null }">
 
         <div class="flex flex-col gap-2">
@@ -102,25 +98,7 @@
 
                         <tbody class="text-gray-600 dark:text-gray-400">
                             @foreach ($data as $key => $user)
-<<<<<<< HEAD
-                                <tr
-                                    class="transition-colors py-10 {{ $loop->iteration % 2 == 0 ? 'bg-gray-100 dark:bg-peak_2' : '' }}">
-                                    <td class=" px-4 align-middle text-black dark:text-white">{{ ++$i }}.</td>
-                                    <td class="px-4 align-middle font-medium">{{ $user->name }}</td>
-                                    <td class="px-4 align-middle">{{ $user->email }}</td>
-                                    <td class="px-4 align-middle">
-                                        @if ($user->getRoleNames()->first() == 'Admin')
-                                            <p class="text-green-500 font-bold ">Admin</p>
-                                        @elseif ($user->getRoleNames()->first() == 'SuperAdmin')
-                                            <p class="text-red-600 font-bold ">SuperAdmin</p>
-                                        @else
-                                            <p class="text-black font-bold dark:text-white ">
-                                                {{ $user->getRoleNames()->first() }}
-                                            </p>
-                                        @endif
-                                    </td>
-=======
-                            <tr
+                            <<<<<<< HEAD <tr
                                 class="transition-colors py-10 {{ $loop->iteration % 2 == 0 ? 'bg-gray-100 dark:bg-peak_2' : '' }}">
                                 <td class=" px-4 align-middle text-black dark:text-white">{{ ++$i }}.</td>
                                 <td class="px-4 align-middle font-medium">{{ $user->name }}</td>
@@ -129,97 +107,116 @@
                                     @if ($user->getRoleNames()->first() == 'Admin')
                                     <p class="text-green-500 font-bold ">Admin</p>
                                     @elseif ($user->getRoleNames()->first() == 'SuperAdmin')
-                                    <p class="text-red-800 font-bold ">SuperAdmin</p>
-                                    @elseif ($user->getRoleNames()->first() == 'Staff')
-                                    <p class="text-yellow-700 font-bold ">Staff</p>
+                                    <p class="text-red-600 font-bold ">SuperAdmin</p>
                                     @else
                                     <p class="text-black font-bold dark:text-white ">
                                         {{ $user->getRoleNames()->first() }}
                                     </p>
                                     @endif
                                 </td>
-                                @canany(['user-view', 'user-edit', 'user-delete'])
-                                <td class="px-4 align-middle">
-                                    <div class="action-dropdown flex items-center justify-center">
-                                        <button
-                                            class="dropdown-button p-1 rounded-md hover:bg-gray-100 dark:hover:bg-peak_3 transition-colors relative">
-                                            <svg class="w-6 h-6 text-gray-800 dark:text-white font-bold"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" fill="none" viewBox="0 0 24 24">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                                                    d="M6 12h.01m6 0h.01m5.99 0h.01" />
-                                            </svg>
-                                        </button>
->>>>>>> 29a4e95c4972173aedb72540c8402f7a5b0bceed
+                                =======
+                                <tr
+                                    class="transition-colors py-10 {{ $loop->iteration % 2 == 0 ? 'bg-gray-100 dark:bg-peak_2' : '' }}">
+                                    <td class=" px-4 align-middle text-black dark:text-white">{{ ++$i }}.</td>
+                                    <td class="px-4 align-middle font-medium">{{ $user->name }}</td>
+                                    <td class="px-4 align-middle">{{ $user->email }}</td>
+                                    <td class="px-4 align-middle">
+                                        @if ($user->getRoleNames()->first() == 'Admin')
+                                        <p class="text-green-500 font-bold ">Admin</p>
+                                        @elseif ($user->getRoleNames()->first() == 'SuperAdmin')
+                                        <p class="text-red-800 font-bold ">SuperAdmin</p>
+                                        @elseif ($user->getRoleNames()->first() == 'Staff')
+                                        <p class="text-yellow-700 font-bold ">Staff</p>
+                                        @else
+                                        <p class="text-black font-bold dark:text-white ">
+                                            {{ $user->getRoleNames()->first() }}
+                                        </p>
+                                        @endif
+                                    </td>
+                                    @canany(['user-view', 'user-edit', 'user-delete'])
+                                    <td class="px-4 align-middle">
+                                        <div class="action-dropdown flex items-center justify-center">
+                                            <button
+                                                class="dropdown-button p-1 rounded-md hover:bg-gray-100 dark:hover:bg-peak_3 transition-colors relative">
+                                                <svg class="w-6 h-6 text-gray-800 dark:text-white font-bold"
+                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                    height="24" fill="none" viewBox="0 0 24 24">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                                        d="M6 12h.01m6 0h.01m5.99 0h.01" />
+                                                </svg>
+                                            </button>
+                                            >>>>>>> 29a4e95c4972173aedb72540c8402f7a5b0bceed
 
-                                        <div
-                                            class="action-dropdown-menu fixed bg-white dark:bg-peak_2 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 z-50 w-48 hidden">
-                                            <div class="py-1">
-                                                @can('user-view')
-                                                <button
-                                                    @click.prevent="openshowmodal = openshowmodal === {{ $user->id }} ? null : {{ $user->id }}"
-                                                    class="w-full group flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-peak_3">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="w-4 h-4 mr-3">
-                                                        <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
-                                                        <circle cx="12" cy="12" r="3" />
-                                                    </svg>
-                                                    View
-                                                </button>
-                                                @endcan
-
-                                                @can('user-edit')
-                                                @if ($user->hasRole('SuperAdmin') && $user->id == 1)
-                                                @else
-                                                <button
-                                                    @click.prevent="openUserId = openUserId === {{ $user->id }} ? null : {{ $user->id }}"
-                                                    class="w-full group flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-peak_3">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                        fill="currentColor" class="w-4 h-4 mr-3" viewBox="0 0 16 16">
-                                                        <path
-                                                            d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z" />
-                                                    </svg>
-                                                    Edit
-                                                </button>
-                                                @endif
-                                                @endcan
-
-                                                @can('user-delete')
-                                                <form method="POST" action="{{ route('users.destroy', $user->id) }}"
-                                                    style="display:inline" class="hidden">
-                                                    @csrf
-                                                    @method('DELETE')
+                                            <div
+                                                class="action-dropdown-menu fixed bg-white dark:bg-peak_2 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 z-50 w-48 hidden">
+                                                <div class="py-1">
+                                                    @can('user-view')
                                                     <button
-                                                        class="w-full group flex items-center px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-peak_3">
+                                                        @click.prevent="openshowmodal = openshowmodal === {{ $user->id }} ? null : {{ $user->id }}"
+                                                        class="w-full group flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-peak_3">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                             viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                             stroke-width="2" stroke-linecap="round"
                                                             stroke-linejoin="round" class="w-4 h-4 mr-3">
-                                                            <path d="M3 6h18"></path>
-                                                            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                                                            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                                                            <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+                                                            <circle cx="12" cy="12" r="3" />
                                                         </svg>
-                                                        Delete
+                                                        View
                                                     </button>
-                                                </form>
-                                                @endcan
+                                                    @endcan
+
+                                                    @can('user-edit')
+                                                    @if ($user->hasRole('SuperAdmin') && $user->id == 1)
+                                                    @else
+                                                    <button
+                                                        @click.prevent="openUserId = openUserId === {{ $user->id }} ? null : {{ $user->id }}"
+                                                        class="w-full group flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-peak_3">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                            fill="currentColor" class="w-4 h-4 mr-3"
+                                                            viewBox="0 0 16 16">
+                                                            <path
+                                                                d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z" />
+                                                        </svg>
+                                                        Edit
+                                                    </button>
+                                                    @endif
+                                                    @endcan
+
+                                                    @can('user-delete')
+                                                    <form method="POST" action="{{ route('users.destroy', $user->id) }}"
+                                                        style="display:inline" class="hidden">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button
+                                                            class="w-full group flex items-center px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-peak_3">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                height="24" viewBox="0 0 24 24" fill="none"
+                                                                stroke="currentColor" stroke-width="2"
+                                                                stroke-linecap="round" stroke-linejoin="round"
+                                                                class="w-4 h-4 mr-3">
+                                                                <path d="M3 6h18"></path>
+                                                                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+                                                                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                                                            </svg>
+                                                            Delete
+                                                        </button>
+                                                    </form>
+                                                    @endcan
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </td>
-                                @endcanany
-                                @can('user-view')
-                                @include ('administrator.users.show')
-                                @endcan
+                                    </td>
+                                    @endcanany
+                                    @can('user-view')
+                                    @include ('administrator.users.show')
+                                    @endcan
 
-                                @can('user-edit')
-                                @include ('administrator.users.edit')
-                                @endcan
+                                    @can('user-edit')
+                                    @include ('administrator.users.edit')
+                                    @endcan
 
-                            </tr>
-                            @endforeach
+                                </tr>
+                                @endforeach
                         </tbody>
                     </table>
                 </div>
