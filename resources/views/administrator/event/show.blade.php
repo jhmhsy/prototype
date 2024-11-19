2e3002c2ed86c403dc1fd339d1665735cd8d8eba
volcanorc <sirjone100@gmail.com>
2024-11-19 14:01:52 +0800

    res6

--- resources/views/administrator/event/show.blade.php
+++ resources/views/administrator/event/show.blade.php
@@ -1,7 +1,7 @@
 <div style="display: none;" x-show="viewmodal" class="fixed select-none inset-0 bg-black opacity-25 z-40">
 </div>
 
-<div style="display: none; width: 450px;"
+<div style="display: none; width: 450px; margin:20px;"
     class="modal fixed w-[90%] md:w-[60%] lg:w-[40%] xl:w-[35%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-4"
     x-show="viewmodal === {{ $event->id}}" @click.away="viewmodal = false"
     x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform scale-90"
