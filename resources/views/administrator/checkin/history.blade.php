d22858f754bb9e87940814ea80df5e6e7090250b
volcanorc <sirjone100@gmail.com>
2024-11-26 00:27:12 +0800

    latest, merge w henry

--- resources/views/administrator/checkin/history.blade.php
+++ resources/views/administrator/checkin/history.blade.php
@@ -1,104 +1,104 @@
 @canany(['checkin-log-list'])
-<x-dash-layout title="Checkin Logs">
-    <div class="container">
-        <h1 class="text-2xl font-bold mb-4 dark:text-white">Check-in History</h1>
+    <x-dash-layout title="Checkin Logs">
+        <div class="container">
+            <h1 class="text-2xl font-bold mb-4 dark:text-white">Check-in History</h1>
 
-        <div class="flex flex-col md:flex-row gap-3 md:gap-5 mb-4">
+            <div class="flex flex-col md:flex-row gap-3 md:gap-5 mb-4">
 
 
-            <div class="w-full  sm:w-[40%] flex items-center ">
-                <span class="text-sm font-medium text-gray-500">Sort by date: </span>
-                <a href="{{ route('checkin.history', ['sort' => 'asc', 'search' => request('search')]) }}"
-                    class="ml-2 inline-flex items-center px-3 py-2 border dark:border-none border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white dark:bg-peak_2 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 {{ $sort == 'asc' ? 'bg-indigo-600 text-white' : '' }}">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 6v13m0-13 4 4m-4-4-4 4" />
-                    </svg>
+                <div class="w-full  sm:w-[40%] flex items-center ">
+                    <span class="text-sm font-medium text-gray-500">Sort by date: </span>
+                    <a href="{{ route('checkin.history', ['sort' => 'asc', 'search' => request('search')]) }}"
+                        class="ml-2 inline-flex items-center px-3 py-2 border dark:border-none border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white dark:bg-peak_2 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 {{ $sort == 'asc' ? 'bg-indigo-600 text-white' : '' }}">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 6v13m0-13 4 4m-4-4-4 4" />
+                        </svg>
 
-                </a>
-                <a href="{{ route('checkin.history', ['sort' => 'desc', 'search' => request('search')]) }}"
-                    class="ml-2 inline-flex items-center px-3 py-2 border dark:border-none border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white dark:bg-peak_2 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 {{ $sort == 'desc' ? 'bg-indigo-600 text-white' : '' }}">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 19V5m0 14-4-4m4 4 4-4" />
-                    </svg>
-                </a>
+                    </a>
+                    <a href="{{ route('checkin.history', ['sort' => 'desc', 'search' => request('search')]) }}"
+                        class="ml-2 inline-flex items-center px-3 py-2 border dark:border-none border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white dark:bg-peak_2 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 {{ $sort == 'desc' ? 'bg-indigo-600 text-white' : '' }}">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 19V5m0 14-4-4m4 4 4-4" />
+                        </svg>
+                    </a>
+                </div>
+
+                <form action="{{ route('checkin.history') }}" method="GET" class="w-full sm:w-[80%] ">
+                    <div class="relative">
+                        <button type="submit"
+                            class="absolute inset-y-0 left-0 flex items-center p-2 text-gray-500 dark:text-gray-400">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                            <span class="sr-only">Search</span> <!-- For accessibility -->
+                        </button>
+                        <input type="text" name="search" maxlength="255" placeholder="Search by Name or ID Number"
+                            value="{{ request('search') }}"
+                            class="block w-full py-2 pl-10 pr-4 text-sm border border-gray-300 rounded-md dark:border-gray-800 dark:bg-peak_2 dark:text-white" />
+                    </div>
+                </form>
             </div>
 
-            <form action="{{ route('checkin.history') }}" method="GET" class="w-full sm:w-[80%] ">
-                <div class="relative">
-                    <button type="submit"
-                        class="absolute inset-y-0 left-0 flex items-center p-2 text-gray-500 dark:text-gray-400">
-                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                        <span class="sr-only">Search</span> <!-- For accessibility -->
-                    </button>
-                    <input type="text" name="search" maxlength="255" placeholder="Search by Name or ID Number"
-                        value="{{ request('search') }}"
-                        class="block w-full py-2 pl-10 pr-4 text-sm border border-gray-300 rounded-md dark:border-gray-800 dark:bg-peak_2 dark:text-white" />
-                </div>
-            </form>
+            <div class="overflow-x-auto">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50 dark:bg-peak_2">
+                        <tr>
+
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Name
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Date
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Time
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Type
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white dark:bg-peak_2 divide-y divide-gray-900 dark:text-white">
+                        @if ($checkins->isEmpty())
+                            <td colspan="8" class="text-center p-3  text-gray-500 mt-4">No check-in logs available at
+                                this time.
+                            </td>
+                        @else
+                            @foreach ($checkins as $checkin)
+                                <tr>
+                                    <td class="px-6 py-4 ">
+                                        <a href="{{ url('members/index?search=' . urlencode($checkin->member->name)) }}"
+                                            class="whitespace-nowrap hover:underline hove">
+                                            {{ $checkin->member->name }}
+                                        </a>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ \Carbon\Carbon::parse($checkin->checkin_date)->format('M j') }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ \Carbon\Carbon::parse($checkin->checkin_time)->format('g:ia') }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ $checkin->type }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+
+            {{ $checkins->appends(['search' => request('search'), 'sort' => $sort])->links() }}
         </div>
-
-        <div class="overflow-x-auto">
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50 dark:bg-peak_2">
-                    <tr>
-
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Name
-                        </th>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Date
-                        </th>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Time
-                        </th>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Type
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white dark:bg-peak_2 e divide-y divide-gray-900 dark:text-white">
-                    @if ($checkins->isEmpty())
-                    <td colspan="8" class="text-center p-3  text-gray-500 mt-4">No check-in logs available at
-                        this time.
-                    </td>
-                    @else
-                    @foreach ($checkins as $checkin)
-                    <tr>
-                        <td class="px-6 py-4 ">
-                            <a href="{{ url('members/index?search=' . urlencode($checkin->member->name)) }}"
-                                class="whitespace-nowrap hover:underline hove">
-                                {{ $checkin->member->name }}
-                            </a>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            {{ \Carbon\Carbon::parse($checkin->checkin_date)->format('M j') }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            {{ \Carbon\Carbon::parse($checkin->checkin_time)->format('g:ia') }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            {{ $checkin->type }}
-                        </td>
-                    </tr>
-                    @endforeach
-                    @endif
-                </tbody>
-            </table>
-        </div>
-
-        {{ $checkins->appends(['search' => request('search'), 'sort' => $sort])->links() }}
-    </div>
-</x-dash-layout>
+    </x-dash-layout>
 @endcanany
